6a636f7da22db1f55064d6be818440d0ef6f3d89
nimA <aminismail2002@gmail.com>
2022-10-12 20:37:51 -0300

    Teste

---
+++ cmake-build-debug/CMakeFiles/BurguerTI_AED.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "BurguerTI_AED"
+  "BurguerTI_AED.pdb"
+  "CMakeFiles/BurguerTI_AED.dir/burger.c.o"
+  "CMakeFiles/BurguerTI_AED.dir/burger.c.o.d"
+  "CMakeFiles/BurguerTI_AED.dir/lista.c.o"
+  "CMakeFiles/BurguerTI_AED.dir/lista.c.o.d"
+  "CMakeFiles/BurguerTI_AED.dir/main.c.o"
+  "CMakeFiles/BurguerTI_AED.dir/main.c.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/BurguerTI_AED.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/BurguerTI_AED.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for BurguerTI_AED.
+# This may be replaced when dependencies are built.
